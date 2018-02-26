69a9384b41fa91660610124b79a969c836f3fbf1
torasku <tor.arve.skutlaberg@gmail.com>
2018-02-26 21:05:49 +0100

    cleanup

--- JSON_test.js
+++
@@ -1,8 +0,0 @@
-function sendText () {
-    var obj = "test";
-    document.getElementById("demo").innerHTML = obj;
-}
-
-function myFunction() {
-    document.getElementById("test").innerHTML = "Paragraph changed!";
-}
